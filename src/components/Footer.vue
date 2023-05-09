849bc7cac96f6e1c8b1386fc24f0d7861aaaccbc
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-05-09 15:59:58 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'Footer'
+
+}
+</script>
+
+
+<template>
+  
+  
+  
+</template>
+
+
+<style lang="scss" scoped>
+@use '../scss/partials/gerneral' as *;
+@use '../scss/partials/variables' as *;
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Header'
+
+}
+</script>
+
+
+<template>
+  
+
+</template>
+
+
+<style lang="scss" scoped>
+@use '../scss/partials/gerneral' as *;
+@use '../scss/partials/variables' as *;
+
+</style>
